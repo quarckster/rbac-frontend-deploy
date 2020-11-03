0b33d0956d07c8727c42ce015a219a76f8b5fae4
Travis CI User <travis@example.org>
2020-11-03 14:41:32 +0000

    use display_name for roles sorting. (#572)

---
+++ js/11.d42d9467b1be2d584149.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{943:function(n,i,s){"use strict";s.r(i);var e=s(0),t=s.n(e);i.default=function(){return t.a.createElement("div",{id:"insights-mock"},"This is just a mock insights bundle module")}}}]);
+//# sourceMappingURL=11.d42d9467b1be2d584149.js.map
