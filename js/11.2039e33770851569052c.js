86d420d32e692cbffa45f7d39b3fe5a2d89cf436
Travis CI User <travis@example.org>
2020-11-05 07:49:10 +0000

    Merge pull request #580 from RedHatInsights/dependabot/npm_and_yarn/redhat-cloud-services/frontend-components-config-2.1.8

--- js/11.2039e33770851569052c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{943:function(n,i,s){"use strict";s.r(i);var e=s(0),t=s.n(e);i.default=function(){return t.a.createElement("div",{id:"insights-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=11.2039e33770851569052c.js.map
