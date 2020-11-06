f6143b1e62475204ece3cf1f8520d1c0f43bcca9
Travis CI User <travis@example.org>
2020-11-06 16:21:56 +0000

    Merge pull request #583 from Hyperkid123/filter-chips-round-two
    Fix group filter chips deletion.

---
+++ js/11.c0487583d0e72b0260ba.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{946:function(n,i,s){"use strict";s.r(i);var e=s(0),t=s.n(e);i.default=function(){return t.a.createElement("div",{id:"insights-mock"},"This is just a mock insights bundle module")}}}]);
+//# sourceMappingURL=11.c0487583d0e72b0260ba.js.map
