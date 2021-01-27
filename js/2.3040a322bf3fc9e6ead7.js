5312fe781d9d9e5c99f2893507dff4f65b4d27af
Travis CI User <travis@example.org>
2021-01-27 08:29:50 +0000

    Merge pull request #647 from PanSpagetka/change-base-path-rbac
    Change base path for RBAC and update links

--- js/2.3040a322bf3fc9e6ead7.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[2],{93002:(e,n,t)=>{"use strict";t.r(n);var r=t(80150),a=t.n(r),c=t(70695),o=t.n(c),s=t(47194),l=t(70983),u=t(54479),i=t(27625),m=t(35240);const b=function(){return a().createElement(l.Provider,{store:u.Z},a().createElement(s.BrowserRouter,{basename:(0,m.eb)(location.pathname)},a().createElement(i.Z,null)))};o().render(a().createElement(b,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=2.3040a322bf3fc9e6ead7.js.map
