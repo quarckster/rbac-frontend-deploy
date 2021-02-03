f02973d7f9eb6ed6d4e7d62ab2b9b82d121975c7
Travis CI User <travis@example.org>
2021-02-03 11:11:05 +0000

    Merge pull request #668 from PanSpagetka/add-access-request-routes
    Add Access request route

---
+++ js/924.38556b5a051e352c6be6.js
@@ -0,0 +1,2 @@
+(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[924],{71924:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>c});var r=t(80150),s=t.n(r);const c=function(){return s().createElement("div",null,"Nothing here yet")}}}]);
+//# sourceMappingURL=924.38556b5a051e352c6be6.js.map
