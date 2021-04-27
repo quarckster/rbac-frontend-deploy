6db514d466682e17582d797a46831c74d42065d0
Travis CI User <travis@example.org>
2021-04-27 12:24:27 +0000

    Merge pull request #773 from Hyperkid123/remove-duplicate-empty-state
    Remove duplicate MUA empty state message.

--- js/6491.8e4d9b936b90a6d053f4.js
+++ js/6491.8e4d9b936b90a6d053f4.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[6491],{36491:(e,n,t)=>{"use strict";t.r(n);var r=t(80150),a=t.n(r),c=t(70695),o=t.n(c),l=t(334),s=t(12167),i=t(43146),u=t(79815),m=t(35240);const b=function(){return a().createElement(s.Provider,{store:i.Z},a().createElement(l.BrowserRouter,{basename:(0,m.eb)(location.pathname,2).includes("rbac")?(0,m.eb)(location.pathname,2):(0,m.eb)(location.pathname,1)},a().createElement(u.Z,null)))};o().render(a().createElement(b,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/6491.8de866d81b015ffdd3a6.js.map
+//# sourceMappingURL=../sourcemaps/6491.86af1bf78d085d74960f.js.map
