5da9ce37a0f9c17de6495d73512914a85823d702
Travis CI User <travis@example.org>
2021-04-29 12:43:35 +0000

    Merge pull request #753 from fhlavac/new-filters-roles
    Persist filters and pagination in Roles

--- js/8446.8ac7b5860543414794c6.js
+++ js/8446.8ac7b5860543414794c6.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8446],{18446:(n,e,r)=>{var s=r(90939);n.exports=function(n,e){return s(n,e)}}}]);
-//# sourceMappingURL=../sourcemaps/8446.86af1bf78d085d74960f.js.map
+//# sourceMappingURL=../sourcemaps/8446.9b69e67a96d06ede8e21.js.map
