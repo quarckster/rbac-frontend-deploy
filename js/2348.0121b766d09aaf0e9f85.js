b507a7c71af2294e17fbadbfb1c3270546c0d06c
Travis CI User <travis@example.org>
2021-05-06 11:20:54 +0000

    Merge pull request #777 from fhlavac/new-filters-groups
    Persist filters and pagination in Groups

--- js/2348.0121b766d09aaf0e9f85.js
+++ js/2348.0121b766d09aaf0e9f85.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[2348],{42348:(n,e,r)=>{var s=r(21078);n.exports=function(n){return null!=n&&n.length?s(n,1/0):[]}}}]);
-//# sourceMappingURL=../sourcemaps/2348.40ffd1c6076c729461ca.js.map
+//# sourceMappingURL=../sourcemaps/2348.b1845bf79fb02fd38792.js.map
