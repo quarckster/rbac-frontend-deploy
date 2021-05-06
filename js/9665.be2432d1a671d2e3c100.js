7be28604db2089a9d08b39fc328babe22d9a3411
Travis CI User <travis@example.org>
2021-05-06 11:45:14 +0000

    Merge pull request #784 from PanSpagetka/bulk-select-permission-fix
    Fix bulk select for Add permissions step in Custom roles

--- js/9665.be2432d1a671d2e3c100.js
+++ js/9665.be2432d1a671d2e3c100.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[9665],{28368:(n,o,i)=>{var r=i(90939);n.exports=function(n,o,i){var t=(i="function"==typeof i?i:void 0)?i(n,o):void 0;return void 0===t?r(n,o,void 0,i):!!t}},30236:(n,o,i)=>{var r=i(42980),t=i(21463)((function(n,o,i,t){r(n,o,i,t)}));n.exports=t}}]);
-//# sourceMappingURL=../sourcemaps/9665.b1845bf79fb02fd38792.js.map
+//# sourceMappingURL=../sourcemaps/9665.c3f793d9b4e7d0778802.js.map
