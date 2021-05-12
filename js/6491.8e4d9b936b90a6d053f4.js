ce7181b55e389e38865541b38a5f96b99b67e097
Travis CI User <travis@example.org>
2021-05-12 06:20:47 +0000

    Merge pull request #787 from PanSpagetka/cost-resource-max-height-for-select
    use custom template for Cost resource step

--- js/6491.8e4d9b936b90a6d053f4.js
+++ js/6491.8e4d9b936b90a6d053f4.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[6491],{36491:(e,n,t)=>{"use strict";t.r(n);var r=t(80150),a=t.n(r),c=t(70695),o=t.n(c),l=t(334),s=t(12167),i=t(43146),u=t(79815),m=t(35240);const b=function(){return a().createElement(s.Provider,{store:i.Z},a().createElement(l.BrowserRouter,{basename:(0,m.eb)(location.pathname,2).includes("rbac")?(0,m.eb)(location.pathname,2):(0,m.eb)(location.pathname,1)},a().createElement(u.Z,null)))};o().render(a().createElement(b,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/6491.0e63fdaa1247e81bff10.js.map
+//# sourceMappingURL=../sourcemaps/6491.cca3fd737c58f8a22a09.js.map
