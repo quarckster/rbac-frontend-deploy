c2b6a691eba2924add958b1f8e90443da90648de
Travis CI User <travis@example.org>
2021-05-20 06:34:16 +0000

    Merge pull request #796 from RedHatInsights/dependabot/npm_and_yarn/patternfly/react-tokens-4.11.8

--- js/8446.8ac7b5860543414794c6.js
+++ js/8446.8ac7b5860543414794c6.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8446],{18446:(n,e,r)=>{var s=r(90939);n.exports=function(n,e){return s(n,e)}}}]);
-//# sourceMappingURL=../sourcemaps/8446.b7770558918449db0273.js.map
+//# sourceMappingURL=../sourcemaps/8446.53301c25616cc1d3607a.js.map
