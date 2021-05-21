946df0df925612116aa4f5f4fda90da93091ee5d
Travis CI User <travis@example.org>
2021-05-21 08:07:33 +0000

    Merge pull request #797 from RedHatInsights/dependabot/npm_and_yarn/redhat-cloud-services/frontend-components-config-4.0.29

--- js/8446.8ac7b5860543414794c6.js
+++ js/8446.8ac7b5860543414794c6.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8446],{18446:(n,e,r)=>{var s=r(90939);n.exports=function(n,e){return s(n,e)}}}]);
-//# sourceMappingURL=../sourcemaps/8446.a166376ed2c4e8d24ad6.js.map
+//# sourceMappingURL=../sourcemaps/8446.53b0704a0518a0b71fdd.js.map
