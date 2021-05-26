8248cfbddff3eef1247c9704e65988ddf28955cf
Travis CI User <travis@example.org>
2021-05-26 11:47:27 +0000

    Fix group name validation & description change (#800)
    * Fix group name validation & description change
    
    * Fix edit group/role modal from detail focus

--- js/2348.0121b766d09aaf0e9f85.js
+++ js/2348.0121b766d09aaf0e9f85.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[2348],{42348:(n,e,r)=>{var s=r(21078);n.exports=function(n){return null!=n&&n.length?s(n,1/0):[]}}}]);
-//# sourceMappingURL=../sourcemaps/2348.227a77502ed312d759f3.js.map
+//# sourceMappingURL=../sourcemaps/2348.ce30b7befa11b2b1ae48.js.map
--- js/8348.e0199671ac1bdde4c20b.js
+++ js/8348.e0199671ac1bdde4c20b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>r});var t=s(80150),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/8348.227a77502ed312d759f3.js.map
+//# sourceMappingURL=../sourcemaps/8348.ce30b7befa11b2b1ae48.js.map
