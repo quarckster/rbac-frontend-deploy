35597f41bb13c835ac592c5a46fc2319b03453a0
Travis CI User <travis@example.org>
2021-06-02 11:25:12 +0000

    Merge pull request #808 from Hyperkid123/fix-schema-error
    Fix undefined schema warning.

--- js/348.787735c482cbd6135747.js
+++ js/348.787735c482cbd6135747.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[348],{18348:(e,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/348.b950137b9158d385f329.js.map
+//# sourceMappingURL=../sourcemaps/348.ff8c6b1bbe800785d795.js.map
