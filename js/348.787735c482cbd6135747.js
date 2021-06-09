2d40f215f7633ba130e49a07617d75ff4ed6ac80
Travis CI User <travis@example.org>
2021-06-09 08:46:14 +0000

    Merge pull request #814 from RedHatInsights/dependabot/npm_and_yarn/patternfly/react-core-4.128.2

--- js/348.787735c482cbd6135747.js
+++ js/348.787735c482cbd6135747.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[348],{18348:(e,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/348.8857f02f3ced9ed7d46b.js.map
+//# sourceMappingURL=../sourcemaps/348.6c627acb3d94f4f03ee6.js.map
