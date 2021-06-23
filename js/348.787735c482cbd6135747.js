dfcd29bac354dc93802bfccee8a546c107ce739e
Travis CI User <travis@example.org>
2021-06-23 11:11:27 +0000

    Merge pull request #830 from RedHatInsights/dependabot/npm_and_yarn/redhat-cloud-services/frontend-components-3.3.2

--- js/348.787735c482cbd6135747.js
+++ js/348.787735c482cbd6135747.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[348],{18348:(e,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/348.4d483fbce6154079fc8a.js.map
+//# sourceMappingURL=../sourcemaps/348.8adb93494363b79e6a29.js.map
