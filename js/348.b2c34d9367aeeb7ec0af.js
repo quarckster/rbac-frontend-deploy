56f0b01528a7b5af0c437c7a9792dc36f279d589
Travis CI User <travis@example.org>
2021-07-16 11:06:42 +0000

    Merge pull request #854 from RedHatInsights/master
    Sync prod-beta

---
+++ js/348.b2c34d9367aeeb7ec0af.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[348],{18348:(e,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
--- js/8446.54263a7f16675a63d536.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8446],{18446:(n,e,r)=>{var s=r(90939);n.exports=function(n,e){return s(n,e)}}}]);
