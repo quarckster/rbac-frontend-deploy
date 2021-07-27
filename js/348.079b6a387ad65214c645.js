13feba9433121dc97401cefd24e961226a10397a
Travis CI User <travis@example.org>
2021-07-27 10:19:24 +0000

    Merge pull request #862 from fhlavac/user-management-link
    Fix the user-management link

--- js/348.079b6a387ad65214c645.js
+++ js/348.079b6a387ad65214c645.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[348],{18348:(e,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/348.dc03d828b1233accdc32.js.map
+//# sourceMappingURL=../sourcemaps/348.23f781467dad7909b058.js.map
