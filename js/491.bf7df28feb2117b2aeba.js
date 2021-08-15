fc0d97e16399d0b6bdeda3d88535e22312821545
Travis CI User <travis@example.org>
2021-08-15 08:26:32 +0000

    Merge pull request #884 from fhlavac/star-fix

--- js/491.bf7df28feb2117b2aeba.js
+++ js/491.bf7df28feb2117b2aeba.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[491],{36491:(e,t,n)=>{"use strict";n.r(t);var r=n(92950),a=n.n(r),o=n(12181),c=n.n(o),l=n(334),s=n(45237),i=n(43146),u=n(79815),m=n(35240);const b=function(){return a().createElement(i.g.Provider,{value:{getRegistry:function(){return i.Z}}},a().createElement(s.Provider,{store:i.Z.getStore()},a().createElement(l.BrowserRouter,{basename:(0,m.eb)(location.pathname,2).includes("rbac")?(0,m.eb)(location.pathname,2):(0,m.eb)(location.pathname,1)},a().createElement(u.Z,null))))};c().render(a().createElement(b,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/491.b0c5b5f13ab391c77c93.js.map
+//# sourceMappingURL=../sourcemaps/491.0659961f45274f9498ce.js.map
