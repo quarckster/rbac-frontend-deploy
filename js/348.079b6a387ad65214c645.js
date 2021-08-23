025449d3271b74af7b2f5030b52eb193bb415e34
Travis CI User <travis@example.org>
2021-08-23 14:39:44 +0000

    Merge pull request #886 from fhlavac/fix-sort
    Fix sorting in TableToolbarView

--- js/348.079b6a387ad65214c645.js
+++ js/348.079b6a387ad65214c645.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[348],{18348:(e,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/348.0659961f45274f9498ce.js.map
+//# sourceMappingURL=../sourcemaps/348.67fb3fa92e1ed5273c99.js.map
