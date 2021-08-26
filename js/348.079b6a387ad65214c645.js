44aa8069af5cc39e44ad78ff6f107004fa628ad1
Travis CI User <travis@example.org>
2021-08-26 07:01:47 +0000

    Merge pull request #890 from epwinchell/new_prefix
    update class names

--- js/348.079b6a387ad65214c645.js
+++ js/348.079b6a387ad65214c645.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[348],{18348:(e,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/348.1d379759f1d5decda65d.js.map
+//# sourceMappingURL=../sourcemaps/348.13fd4192599c77142334.js.map
