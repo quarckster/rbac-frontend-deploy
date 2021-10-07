5f8395b75343b03b7a1153c42ad7ede288c6ee12
Travis CI User <travis@example.org>
2021-10-07 13:06:00 +0000

    Merge pull request #938 from Hyperkid123/master
    Move router pathnames defition to a JS file.

---
+++ js/337.6d59dfd8d7bc8ecfb6b2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[337],{35337:(s,n,e)=>{e.r(n)}}]);
--- js/337.a16fc45f412dcc20addd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[337],{35337:(s,n,e)=>{"use strict";e.r(n)}}]);
