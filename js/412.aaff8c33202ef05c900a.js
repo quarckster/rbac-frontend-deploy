11094bae356375f5fb754d607dec5603f77f9727
Travis CI User <travis@example.org>
2021-11-02 15:54:07 +0000

    Merge pull request #960 from Hyperkid123/master
    Triggering akamai upload.

--- js/412.aaff8c33202ef05c900a.js
+++ js/412.aaff8c33202ef05c900a.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>u});var r=n(92950),a=n.n(r),c=n(334),o=n(45237),s=n(43146),i=n(49289),l=n(35240);const u=function(){return a().createElement(s.g.Provider,{value:{getRegistry:function(){return s.Z}}},a().createElement(o.Provider,{store:s.Z.getStore()},a().createElement(c.BrowserRouter,{basename:(0,l.eb)(location.pathname,2).includes("rbac")?(0,l.eb)(location.pathname,2):(0,l.eb)(location.pathname,1)},a().createElement(i.Z,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/412.d404e649ecfa1e30ace27e665d82b448.js.map
+//# sourceMappingURL=../sourcemaps/412.29273985e0d5dd3320592053fa824e42.js.map
